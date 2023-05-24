28ac96a3f372c0f31f5da0eae4d59b34424fb2ef
Robin Tapiru <robintapiru0894@gmail.com>
2023-05-24 18:05:31 +0400

    todo list

---
+++ src/components/Todo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import TodoForm from "./TodoForm";
+
+export default function todo() {
+  return (
+    <div>
+      <h1>Add your to do list</h1>
+      <TodoForm />
+    </div>
+  );
+}
